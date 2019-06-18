372bd1ff7af18cdbcb181b477e8c73ef675f99dd
Gerald Diesenreiter <S1810237011@students.fh-hagenberg.at>
2019-06-18 09:59:34 +0200

    new: dynamic list next try

--- app/src/main/java/at/fhooe/mc/android/Person.java
+++ app/src/main/java/at/fhooe/mc/android/Person.java
@@ -2,10 +2,21 @@ package at.fhooe.mc.android;
 
 public class Person {
     private String mName;
-    private int mExpenditures;
+    private int mExpenditures; //Ausgaben
 
     public Person(String name){
         mName = name;
         mExpenditures = 0;
     }
+
+    public String getName(){
+        return mName;
+    }
+
+    public int getExpenditures(){
+        return mExpenditures;
+    }
+
+
+
 }
