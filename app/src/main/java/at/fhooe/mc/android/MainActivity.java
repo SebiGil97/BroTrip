0c3f817c41fca6acac025480410519a0bbf663ec
Gerald Diesenreiter <S1810237011@students.fh-hagenberg.at>
2019-06-18 09:45:28 +0200

    new: dynamic list

--- app/src/main/java/at/fhooe/mc/android/MainActivity.java
+++ app/src/main/java/at/fhooe/mc/android/MainActivity.java
@@ -2,21 +2,29 @@ package at.fhooe.mc.android;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static at.fhooe.mc.android.ActivityNewTrip.KEY_CAR;
+import static at.fhooe.mc.android.ActivityNewTrip.KEY_TITLE;
+
 public class MainActivity extends Activity implements View.OnClickListener {
 
-    public static final String TAG = "BroTrip";
-    public int mNumberOfTrips = 0;
-    public String[] mTripList = new String[]{};
+    public static final String TAG          = "BroTrip";
+    private static final String SP_KEY      = "ActivityNewTrip_NewTrip";
+
+    public Trip[] mTripList = new Trip[]{};
 
 
 
@@ -31,35 +39,58 @@ public class MainActivity extends Activity implements View.OnClickListener {
         b.setOnClickListener(this);
         b = (Button) findViewById(R.id.activity_main_button_test_list);
         b.setOnClickListener(this);
+        Button Load = (Button) findViewById(R.id.activity_main_button_load_test);
+        b.setOnClickListener(this);
 
 
         //---------- Dynamic List ----------
         ListView lv = (ListView) findViewById(R.id.activity_main_listView_trips);
 
-        final List<String> TripArrayList = new ArrayList<String>(Arrays.asList(mTripList));
+        final List<Trip> TripArrayList = new ArrayList<Trip>(Arrays.asList(mTripList));
 
-        //TripDataAdapter adapter = new TripDataAdapter(this); // which Context and how to use the selfmade adapter
+        final TripDataAdapter adapter = new TripDataAdapter(this); // which Context and how to use the selfmade adapter
 
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, TripArrayList);
+
+        //final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, TripArrayList);
 
         lv.setAdapter(adapter);
+
+       final SharedPreferences sp = getSharedPreferences(SP_KEY, MODE_PRIVATE);
+        Load.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) { // soll automatisch funktionieren und nur einmal
+
+                String title = sp.getString(KEY_TITLE, "undefined"); // holt sich String title
+                String car = sp.getString(KEY_CAR, "undefined"); // holt sich String car
+                Trip trip = new Trip(title, car);
+
+
+                adapter.add(trip);
+                Toast.makeText(MainActivity.this, "load " + trip.getTripTitle() + " & " + trip.getCar(), Toast.LENGTH_SHORT).show();
+
+                adapter.notifyDataSetChanged();
+
+            }
+        });
     }
 
     @Override
     public void onClick(View _v) {
+
+
         switch(_v.getId()){
 
             case R.id.activity_main_button_new_trip : {
-            Intent i = new Intent(this, ActivityNewTrip.class);
-            startActivity(i);
+                Intent i = new Intent(this, ActivityNewTrip.class);
+                startActivity(i);
             } break;
 
             case R.id.activity_main_button_test_list : {
-
                 Intent i = new Intent(this, ActivityActiveTrip.class);
                 startActivity(i);
-
             } break;
+
             default : Log.e(TAG, "unexpected ID encountered");
         }
     }
--- app/src/main/java/at/fhooe/mc/android/TripDataAdapter.java
+++ app/src/main/java/at/fhooe/mc/android/TripDataAdapter.java
@@ -1,31 +1,46 @@
 package at.fhooe.mc.android;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class TripDataAdapter extends ArrayAdapter<Trip> {
+
 
     public TripDataAdapter(Context _c){
         super(_c,-1);
     }
 
     public View getView(int _position, View _convertView, ViewGroup _parent){
+
+        final Trip data = getItem(_position);
+
         if(_convertView == null){
+
             LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             _convertView = inflater.inflate(R.layout.list_trip, null);
 
         }
-        Trip data = getItem(_position);
 
         TextView tv = null;
         tv = (TextView)_convertView.findViewById(R.id.list_trip_textView_trip_title);
         tv.setText(data.getTripTitle());
         tv = (TextView)_convertView.findViewById(R.id.list_trip_textView_car);
         tv.setText(data.getCar());
+
+        Button b = (Button) _convertView.findViewById(R.id.list_trip_button_settings);
+        b.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                 Toast.makeText(getContext(), data.getTripTitle() + " options", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return _convertView;
     }
